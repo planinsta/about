9a744245edf13d1cd6852777602167ea2fddd5b1
ilievlad73 <ilievlad73@gmail.com>
2019-10-27 01:25:33 +0300

    Updates

---
+++ precache-manifest.df5eaf12336ea1530939f3d42ae4ca24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28cdeb03fbdad6ed403d4203b7644b6e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "09a0f77882ef4ba75ebc",
+    "url": "/home/static/css/main.05f22097.chunk.css"
+  },
+  {
+    "revision": "e4a7f0bc6f6f767bd83b",
+    "url": "/home/static/js/2.cfa1e127.chunk.js"
+  },
+  {
+    "revision": "09a0f77882ef4ba75ebc",
+    "url": "/home/static/js/main.6c842ac3.chunk.js"
+  },
+  {
+    "revision": "61928bcb4b4102fdc79c",
+    "url": "/home/static/js/runtime-main.9914e1ff.js"
+  },
+  {
+    "revision": "e6bf7f421f20912f8fd051aa748799a2",
+    "url": "/home/static/media/fundal.e6bf7f42.jpg"
+  },
+  {
+    "revision": "3417a6c5de9f26b5002769c3f3e298d3",
+    "url": "/home/static/media/salon.3417a6c5.jpg"
+  }
+]);
