d0121f954baa4dc18600d532c70724f351fc0872
ilievlad73 <ilievlad73@gmail.com>
2019-10-27 02:27:38 +0300

    Updates

---
+++ precache-manifest.8e8ae74986ac54c4d26dea906fe189b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd21fd55fb52ae091401551a9eec0fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a6388401e17b2902b93",
+    "url": "/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "03a9ab86c06c0ee4d7c6",
+    "url": "/static/css/main.05f22097.chunk.css"
+  },
+  {
+    "revision": "0a6388401e17b2902b93",
+    "url": "/static/js/2.4d1653d2.chunk.js"
+  },
+  {
+    "revision": "03a9ab86c06c0ee4d7c6",
+    "url": "/static/js/main.f61422aa.chunk.js"
+  },
+  {
+    "revision": "72fda174455962a1e339",
+    "url": "/static/js/runtime-main.62493ae3.js"
+  },
+  {
+    "revision": "e6bf7f421f20912f8fd051aa748799a2",
+    "url": "/static/media/fundal.e6bf7f42.jpg"
+  },
+  {
+    "revision": "06407981cf56dd7fe0b011ca0203baf5",
+    "url": "/static/media/planinsta_solutie.06407981.mp4"
+  },
+  {
+    "revision": "3417a6c5de9f26b5002769c3f3e298d3",
+    "url": "/static/media/salon.3417a6c5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1db9a03274fffdd2350ea8cf6e03e9c4.js"
+  "/precache-manifest.8e8ae74986ac54c4d26dea906fe189b0.js"
 );
 
 self.addEventListener('message', (event) => {
