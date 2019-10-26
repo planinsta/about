e3b9ba607e30a5b1c3c2d15843571bed2dd6661c
ilievlad73 <ilievlad73@gmail.com>
2019-10-27 00:48:10 +0300

    Deployed first working landing page. Contains only landing page section for client profile

---
+++ precache-manifest.2ca78a181b64058d5831aca4c1c1c489.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29dab0fb511eddf02b48d3de4b2bb90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e76d8ac53f7bcd35a08",
+    "url": "/static/css/main.05f22097.chunk.css"
+  },
+  {
+    "revision": "e4a7f0bc6f6f767bd83b",
+    "url": "/static/js/2.cfa1e127.chunk.js"
+  },
+  {
+    "revision": "3e76d8ac53f7bcd35a08",
+    "url": "/static/js/main.22cca287.chunk.js"
+  },
+  {
+    "revision": "72fda174455962a1e339",
+    "url": "/static/js/runtime-main.62493ae3.js"
+  },
+  {
+    "revision": "e6bf7f421f20912f8fd051aa748799a2",
+    "url": "/static/media/fundal.e6bf7f42.jpg"
+  },
+  {
+    "revision": "3417a6c5de9f26b5002769c3f3e298d3",
+    "url": "/static/media/salon.3417a6c5.jpg"
+  }
+]);
--- src/theme.js
+++
@@ -1,7 +0,0 @@
-export default {
-  blue: "#0a3d62",
-  red: "#e55039",
-  heights: {
-    navBar: 50
-  }
-};
